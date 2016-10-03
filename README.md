81c91a2e4ad0c6c2523569b2b17b6ebaf72742f8
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2016-10-03 13:54:28 -0400

    edit readme re-deploy to heroku

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
-# week-3-game
+## Saturday Morning Cartoons - Hangman
 
-Go to play: http://hidden-hamlet-26406.herokuapp.com/
+Hangman game with a Saturday Morning Cartoon theme. Click enter to begin the game. Input letters until you solve the puzzle. When you solve it, it tracks your wins and plays the theme music for that cartoon until you guess the next letter for the next puzzle. Also, tracks your losses if you don't solve the puzzle in less than 12 guesses.
+
+## Motivation
+
+Created during Week 3 of Rutgers Coding Bootcamp. The challenge was to create a hangman game. I decided to go with a 90s Cartoon Theme and therefore designed the app with the 90s patterns and bright neon color. We were to add music (if there was time) whenever the user solved the puzzle.
+
+## Screenshots
+
+![Starting Screen Shot](images/ScreenShot1.png)
+Starting the game.
+
+![Starting Screen Shot](images/ScreenShot2.png)
+Tracks letters guessed and lets the user know if they've already guessed a letter.
+
+## Technologies used
+- Google Fonts
+- Bootstrap
+- jQuery
+
+## Getting Started 
+https://git.heroku.com/cartoon-hangman
+
+### Prerequisities
+
+```
+- Bootstrap: visit http://getbootstrap.com/ and link in html
+- Google Fonts: visit https://fonts.google.com/ and link to html. Copy code provided and implement into css file
+- jQuery
+```
+
+## Built With
+
+* Sublime Text - Text Editor
+* Bootstrap - Wireframe
+
+## Authors
+
+* **Stefanie Ding** - *HTML/CSS/JS* - [Stefanie Ding](https://github.com/StefanieDing)
+
+## Acknowledgments
+
+* Thanks to Dan, Nate, and Jimmy from Rutgers Coding Bootcamp for additional help.
--- javascript/game.js
+++ javascript/game.js
@@ -4,14 +4,14 @@ var lives = 12;
 var winCounter = 0;
 var loseCounter = 0;
 var messages = {
-	win: 'Awesome!!! You won!',
-	lose: 'You lose. Game over.',
-	guessed: 'You guessed that letter already! Try again.',
-	validLetter: 'Enter a letter from A - Z',
+    win: 'Awesome!!! You won!',
+    lose: 'You lose. Game over.',
+    guessed: 'You guessed that letter already! Try again.',
+    validLetter: 'Enter a letter from A - Z',
 }
 var isLetter = false;
 var alreadyGuessed = false;
-var lettersGuessed = []; 
+var lettersGuessed = [];
 var guesses = 0;
 var currentWord;
 var hiddenWord = [];
@@ -20,147 +20,139 @@ var winnerWinner = true;
 var wordInt;
 //Music
 var dir = 'music/';
-var playlist = ['Recess','Animaniacs','Transformers','Pokemon','Doug','DexsLab','Hey-Arnold','KP','PPG','Samurai-Jack','LooneyTunes','ScoobyDoo','Batman','CatDog','PepperAnn','Superman','CareBears','MLP','DuckTales','Talespin','RockoModLife','Rugrats','AngryBeavers'];
+var playlist = ['Recess', 'Animaniacs', 'Transformers', 'Pokemon', 'Doug', 'DexsLab', 'Hey-Arnold', 'KP', 'PPG', 'Samurai-Jack', 'LooneyTunes', 'ScoobyDoo', 'Batman', 'CatDog', 'PepperAnn', 'Superman', 'CareBears', 'MLP', 'DuckTales', 'Talespin', 'RockoModLife', 'Rugrats', 'AngryBeavers'];
 var ext = '.mp3';
 var audio = new Audio();
 
-function newGame(){
-	wordInt= Math.floor((Math.random() * words.length));
-	currentWord = words[wordInt]; 
-	console.log(currentWord);
+function newGame() {
+    wordInt = Math.floor((Math.random() * words.length));
+    currentWord = words[wordInt];
+    console.log(currentWord);
 
-	if (hiddenWord.length !== currentWord.length){
-		hiddenWord = [];
-	}
+    if (hiddenWord.length !== currentWord.length) {
+        hiddenWord = [];
+    }
 
-	//replace characters with blanks
-	for (var i = 0; i < currentWord.length; i++){
-		if(currentWord[i] === " "){
-		hiddenWord[i] = "  ";
-		}
-		else{
-		hiddenWord[i] = (" _ ");
-		}
-	}
+    //replace characters with blanks
+    for (var i = 0; i < currentWord.length; i++) {
+        if (currentWord[i] === " ") {
+            hiddenWord[i] = "  ";
+        } else {
+            hiddenWord[i] = (" _ ");
+        }
+    }
 
-	$('#hiddenWord').html(hiddenWord);
+    $('#hiddenWord').html(hiddenWord);
 }
 
-	//Records keyboard input
-document.onkeyup = function(event){
-	var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
-	var enter = (event.keyCode);
+//Records keyboard input
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
+    var enter = (event.keyCode);
 
-	audio.pause();
-	//Press Enter to start Game
-	if (enter == 13){
-		newGame();
-	}
+    audio.pause();
+    //Press Enter to start Game
+    if (enter == 13) {
+        newGame();
+    }
 
-	//Check to see if letter, if not send message: validLetter
-	for(var i = 0; i < alphabet.length; i++){
-		if(userGuess === alphabet.charAt(i)){
-			isLetter = true;
-		}
-	}	
+    //Check to see if letter, if not send message: validLetter
+    for (var i = 0; i < alphabet.length; i++) {
+        if (userGuess === alphabet.charAt(i)) {
+            isLetter = true;
+        }
+    }
 
-	if(isLetter == false && enter != 13){
-		$('#messages').html(messages.validLetter);
-	}
+    if (isLetter == false && enter != 13) {
+        $('#messages').html(messages.validLetter);
+    }
 
-	//If letter was guessed already send message: guessed
-	for(var i = 0; i < lettersGuessed.length; i++){
-		if(userGuess === lettersGuessed[i]){
-			alreadyGuessed = true;
-		}
-	}
+    //If letter was guessed already send message: guessed
+    for (var i = 0; i < lettersGuessed.length; i++) {
+        if (userGuess === lettersGuessed[i]) {
+            alreadyGuessed = true;
+        }
+    }
 
-	//If already in the clue, then returns alert saying guessed already
-	for(var i = 0; i < hiddenWord.length; i++){
-		if (userGuess == hiddenWord[i]){
-				alreadyGuessed = true;
-		}
-	}
+    //If already in the clue, then returns alert saying guessed already
+    for (var i = 0; i < hiddenWord.length; i++) {
+        if (userGuess == hiddenWord[i]) {
+            alreadyGuessed = true;
+        }
+    }
 
-	if(alreadyGuessed == true){
-			$('#messages').html(messages.guessed);
-		}
+    if (alreadyGuessed == true) {
+        $('#messages').html(messages.guessed);
+    }
 
-	//Checks for letter match and inserts into hiddenWord array
-	for(var i = 0; i < currentWord.length; i++){
-		if (currentWord[i] === userGuess){
-			hiddenWord[i] = currentWord[i];
-			rightLetter = true;
-		}
-	}	
+    //Checks for letter match and inserts into hiddenWord array
+    for (var i = 0; i < currentWord.length; i++) {
+        if (currentWord[i] === userGuess) {
+            hiddenWord[i] = currentWord[i];
+            rightLetter = true;
+        }
+    }
 
-	//Pushes letters guess into an array and takes away a life for wrong letter. 
-	if(isLetter == true && alreadyGuessed == false && rightLetter == false){
-		lettersGuessed.push(userGuess);
-		lives--;
-		$('#lives').html(lives);
-	}
+    //Pushes letters guess into an array and takes away a life for wrong letter. 
+    if (isLetter == true && alreadyGuessed == false && rightLetter == false) {
+        lettersGuessed.push(userGuess);
+        lives--;
+        $('#lives').html(lives);
+    }
 
-	$('#lettersGuessed').html(lettersGuessed);
-	$('#hiddenWord').html(hiddenWord);
+    $('#lettersGuessed').html(lettersGuessed);
+    $('#hiddenWord').html(hiddenWord);
 
-	//When out of lives - user loses and counter goes up
-	if(lettersGuessed.length == 12){
-			$('#messages').html(messages.lose);
-		lettersGuessed = [];
-			$('#lettersGuessed').html(lettersGuessed);
-		loseCounter++;
-			$('#loseCounter').html(loseCounter);
-		lives = 12;
-		newGame();
-	}
+    //When out of lives - user loses and counter goes up
+    if (lettersGuessed.length == 12) {
+        $('#messages').html(messages.lose);
+        lettersGuessed = [];
+        $('#lettersGuessed').html(lettersGuessed);
+        loseCounter++;
+        $('#loseCounter').html(loseCounter);
+        lives = 12;
+        newGame();
+    }
 
-	//Resets booleans between keystrokes.
-	rightLetter = false;
-	isLetter = false;
-	alreadyGuessed = false;
+    //Resets booleans between keystrokes.
+    rightLetter = false;
+    isLetter = false;
+    alreadyGuessed = false;
 
-	//Asks if the user has won
-	if(enter != 13){
-		win();
-	}
-}	
-
-function win(){
-
-	for(var i = 0; i < currentWord.length; i++){
-		if (hiddenWord[i] == " _ "){
-			winnerWinner = false;
-		}
-	}
-
-	if (winnerWinner == true){
-			$('#messages').html(messages.win);
-
-//plays music after you win to the corresponding word *WORKING
-		for(var i = 0; i < playlist.length; i++){
-		 	if(wordInt === i){
-		 		audio.src= dir+playlist[i]+ext;
-		 		audio.play();
-		 	}
-		}
-		
-		winCounter++;
-			$('#winCounter').html(winCounter);
-		lettersGuessed = [];
-			$('#lettersGuessed').html(lettersGuessed);
-		lives = 12;
-			$('#lives').html(lives);
-
-		newGame();
-	}
-
-	winnerWinner = true;
+    //Asks if the user has won
+    if (enter != 13) {
+        win();
+    }
 }
 
+function win() {
 
+    for (var i = 0; i < currentWord.length; i++) {
+        if (hiddenWord[i] == " _ ") {
+            winnerWinner = false;
+        }
+    }
 
-	
+    if (winnerWinner == true) {
+        $('#messages').html(messages.win);
 
+        //plays music after you win to the corresponding word *WORKING
+        for (var i = 0; i < playlist.length; i++) {
+            if (wordInt === i) {
+                audio.src = dir + playlist[i] + ext;
+                audio.play();
+            }
+        }
 
+        winCounter++;
+        $('#winCounter').html(winCounter);
+        lettersGuessed = [];
+        $('#lettersGuessed').html(lettersGuessed);
+        lives = 12;
+        $('#lives').html(lives);
 
+        newGame();
+    }
+
+    winnerWinner = true;
+}
